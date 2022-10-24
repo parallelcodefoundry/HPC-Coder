cd99ee69f8bee1297c6630d894e933824423d6cb
Daniel Nichols <dando18studios@gmail.com>
2022-10-24 12:13:16 -0400

    refactor data copy scripts

--- analysis/clean-data.bash
+++ analysis/clean-data.bash
@@ -1,18 +1,32 @@
 #!/bin/bash
 # Data cannot persist in scratch space after training job. Clear these out from login node.
+# THIS SCRIPT IS SPECIFIC TO ZARATAN.
 # author: Daniel Nichols
 # date: October 2022
 
-if [ $# -eq 1 ]; then
-    DST="$1"
-else
-    DST="/scratch/zt1/project/bhatele-lab/user/dnicho/code-ml/data/dataset.jsonl"
+# remove dataset
+echo "Removing dataset..."
+rm /scratch/zt1/project/bhatele-lab/user/dnicho/code-ml/data/dataset.json
+if [ $? -ne 0 ]; then
+    echo "Error deleting dataset!"
 fi
 
-# remove dataset
-echo "rm $DST"
-rm $DST
-
 # remove cache
-echo "rm -r /scratch/zt1/project/bhatele-lab/user/dnicho/.cache/huggingface/datasets/*"
-rm -r /scratch/zt1/project/bhatele-lab/user/dnicho/.cache/huggingface/datasets/*
+echo "Saving and removing cache..."
+cp -r /scratch/zt1/project/bhatele-lab/user/dnicho/.cache/huggingface \
+    /afs/shell.umd.edu/project/bhatele-lab/user/dnicho/.cache
+if [ $? -eq 0 ]; then
+    rm -r /scratch/zt1/project/bhatele-lab/user/dnicho/.cache/huggingface
+else
+    echo "Error copying .cache directory!"
+fi
+
+# move model checkpoints
+echo "Saving and removing saved models..."
+cp -r /scratch/zt1/project/bhatele-lab/user/dnicho/code-ml/models \
+    /afs/shell.umd.edu/project/bhatele-lab/user/dnicho/code-ml/
+if [ $? -eq 0 ]; then
+    rm -r /scratch/zt1/project/bhatele-lab/user/dnicho/code-ml/models
+else
+    echo "Error copying saved models!"
+fi
--- analysis/prepare-data.bash
+++ analysis/prepare-data.bash
@@ -1,16 +1,32 @@
 #!/bin/bash
 # Initial setup for training requires some work to be done on login node. Namely the dataset has to be copied
 # to scratch space for the job to access it.
+# THIS SCRIPT IS SPECIFIC TO ZARATAN.
 # author: Daniel Nichols
 # date: October 2022
 
 SRC="/afs/shell.umd.edu/project/bhatele-lab/shared/hpc-llms/data/dataset.jsonl"
-DST="/scratch/zt1/project/bhatele-lab/user/dnicho/code-ml/data/dataset.jsonl"
+DST="/scratch/zt1/project/bhatele-lab/user/dnicho/code-ml/data/dataset.json"
 
 # copy dataset
-echo "cp ${SRC} ${DST}"
+echo "Copying dataset..."
 cp ${SRC} ${DST}
+if [ $? -ne 0 ]; then
+      echo "Error copying dataset!"
+fi
 
 # create huggingface cache
-echo "mkdir -p /scratch/zt1/project/bhatele-lab/user/dnicho/.cache/huggingface/datasets"
-mkdir -p /scratch/zt1/project/bhatele-lab/user/dnicho/.cache/huggingface/datasets
+echo "Copying huggingface cache..."
+cp -r /afs/shell.umd.edu/project/bhatele-lab/user/dnicho/.cache/huggingface \
+      /scratch/zt1/project/bhatele-lab/user/dnicho/.cache/
+if [ $? -ne 0 ]; then
+      echo "Error copying huggingface cache!"
+fi
+
+# move save dirs
+echo "Copying saved models..."
+cp -r /afs/shell.umd.edu/project/bhatele-lab/user/dnicho/code-ml/models \
+    /scratch/zt1/project/bhatele-lab/user/dnicho/code-ml/
+if [ $? -ne 0 ]; then
+      echo "Error copying saved models!"
+fi
