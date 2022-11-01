67953638b08d0ea4b693814bd24bf5660cb85b93
Daniel Nichols <dando18studios@gmail.com>
2022-11-01 15:57:25 -0400

    updated text generation scripts

--- analysis/generate_text.py
+++ analysis/generate_text.py
@@ -4,6 +4,7 @@
 '''
 # std imports
 from argparse import ArgumentParser
+from os import environ
 
 # tpl imports
 from transformers import pipeline
@@ -11,34 +12,63 @@ from transformers import pipeline
 
 def main():
     parser = ArgumentParser(description='Generate text from a prompt using a LLM.')
-    input_group = parser.add_mutually_exclusive_group(required=True)
+    input_group = parser.add_mutually_exclusive_group()
     input_group.add_argument('--text', type=str, help='Input text to generate new text from.')
     input_group.add_argument('--text-file', type=str, help='File to get text contents from.')
+    parser.add_argument('-n', '--num-samples', type=int, default=10, help='How many times to sample a particular input.')
     parser.add_argument('--model', type=str, required=True, help='Huggingface hub model name or path to model.')
+    parser.add_argument('--tokenizer', type=str, required=True, help='Tokenizer to use on data.')
     parser.add_argument('--min-len', type=int, default=50, help='Minimum length to generate.')
+    parser.add_argument('--max-len', type=int, default=150, help='Maximum length to generate.')
+    parser.add_argument('--device', type=int, default=-1, help='Where to run model')
     parser.add_argument('-o', '--output', type=str, default='-', help='Output location. Omit or \'-\' for stdout.')
     args = parser.parse_args()
 
+    # environment setup
+    environ['TOKENIZERS_PARALLELISM'] = '0'
+    environ['OMP_NUM_THREADS'] = '64'
+
     # get text data
     prompt = ''
+    reprompt = False
     if args.text:
         prompt = args.text
-    else:
+    elif args.text_file:
         with open(args.text_file, 'r', errors='ignore') as fp:
             prompt = fp.read()
+    else:
+        reprompt = True
+        prompt = input('prompt: ')
     
     # create pipeline and generate
-    generator = pipeline('text-generation', model=args.model)
-    result = generator(prompt, do_sample=True, min_length=args.min_len)
-    print(result)
+    generator = pipeline('text-generation', model=args.model, tokenizer=args.tokenizer, framework='pt', device=args.device)
+    
+    if reprompt:
+        while prompt not in ['q', 'quit', 'exit']:
+            prompt = prompt.strip()
+            if prompt == '':
+                prompt = input('prompt: ')
+                continue
 
-    # output
-    response = result[0]['generated_text']
-    if args.output is None or args.output == '-':
-        print(response)
+            prompts = [prompt] * args.num_samples
+            result = generator(prompts, do_sample=True, min_length=args.min_len, max_new_tokens=args.max_len)
+            print(result)
+            print(result[0]['generated_text'])
+
+            prompt = input('prompt: ')
     else:
-        with open(args.output, 'w') as fp:
-            fp.write(response)
+        prompts = [prompt] * args.num_samples
+        result = generator(prompts, do_sample=True, min_length=args.min_len, max_new_tokens=args.max_len)
+    
+        # output
+        response = result#[0]['generated_text']
+        for idx, resp in enumerate(response, start=1):
+            gen_text = resp[0]['generated_text']
+            if args.output is None or args.output == '-':
+                print('Sample {}: \'{}\'\n'.format(idx, gen_text))
+            else:
+                with open(args.output, 'w') as fp:
+                    fp.write('Sample {}: \'{}\'\n'.format(idx, gen_text))
 
 
 if __name__ == '__main__':
