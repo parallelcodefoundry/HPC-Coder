992eb1986ccd302a2ad3ccc1d3be02b92cdcf961
Daniel Nichols <dando18studios@gmail.com>
2022-11-03 12:11:25 -0400

    update omp collect script

--- data/create-omp-dataset.py
+++ data/create-omp-dataset.py
@@ -5,8 +5,12 @@
 # std imports
 from argparse import ArgumentParser
 import json
+from os import PathLike
 import re
 from typing import Iterable, Optional
+
+# tpl imports
+from alive_progress import alive_bar
 
 
 PATTERN = r'\#pragma omp parallel for.*'
@@ -40,6 +44,8 @@ def get_omp_samples(
     data_sample : dict, 
     pre_loop_token : Optional[str] = '', 
     post_loop_token : Optional[str] = '',
+    pre_pragma_token : Optional[str] = '',
+    post_pragma_token : Optional[str] = '',
     lines_before : Optional[int] = None,
     chars_before : Optional[int] = None
 ) -> Iterable[dict]:
@@ -102,18 +108,37 @@ def get_omp_samples(
 
         new_sample = { k: v for k, v in data_sample.items() if k != 'text' }
         new_sample['omp_pragma_line'] = omp_text
-        new_sample['text'] = context + loop_text + ' ' + omp_text
+        new_sample['context_chars'] = chars_before
+        new_sample['text'] = context + loop_text + ' ' + pre_pragma_token + omp_text + post_pragma_token
         new_samples.append( new_sample )
 
     return new_samples
+
+
+def count_lines(fpath : PathLike) -> int:
+    ''' Count the number of lines in a file.
+
+        Args:
+            fpath: path to input file
+        
+        Returns:
+            number of lines in fpath
+    '''
+    lines = 0
+    with open(fpath, 'r') as fp:
+        for _ in fp:
+            lines += 1
+    return lines 
 
 
 def main():
     parser = ArgumentParser(description='Script that creates the openmp auto-complete dataset.')
     parser.add_argument('-i', '--input', type=str, required=True, help='path to jsonl dataset')
     parser.add_argument('-o', '--output', type=str, required=True, help='path to output dataset from script')
-    parser.add_argument('--pre-loop-token', type=str, default='<LOOP START>', help='Token to add before loop.')
-    parser.add_argument('--post-loop-token', type=str, default='<LOOP END>', help='Token to add after loop.')
+    parser.add_argument('--pre-loop-token', type=str, default='<LOOP-START>', help='Token to add before loop.')
+    parser.add_argument('--post-loop-token', type=str, default='<LOOP-END>', help='Token to add after loop.')
+    parser.add_argument('--pre-pragma-token', type=str, default='<OMP-START>', help='Token to add before omp pragma.')
+    parser.add_argument('--post-pragma-token', type=str, default='<OMP-END>', help='Token to add after omp pragma.')
     context_group = parser.add_mutually_exclusive_group(required=True)
     context_group.add_argument('--num-chars-context', type=int, help='how many chars before loop to include')
     context_group.add_argument('--num-lines-context', type=int, help='how many lines before loop to include')
@@ -121,17 +146,21 @@ def main():
 
     # process online online, since dataset may be large
     count = 0
-    with open(args.input, 'r') as fp_in, open(args.output, 'w') as fp_out:
+    with open(args.input, 'r') as fp_in, open(args.output, 'w') as fp_out, \
+        alive_bar(count_lines(args.input), title='Finding OpenMP For Loops') as bar:
         for line in fp_in:
             sample = json.loads(line)
             new_samples = get_omp_samples(sample, 
                 pre_loop_token=args.pre_loop_token, post_loop_token=args.post_loop_token,
+                pre_pragma_token=args.pre_pragma_token, post_pragma_token=args.post_pragma_token,
                 lines_before=args.num_lines_context, chars_before=args.num_chars_context)
 
             count += len(new_samples)
             for sample in new_samples:
                 json.dump(sample, fp_out, ensure_ascii=False)
                 fp_out.write('\n')
+            
+            bar()
 
     
     print(f'Found {count} total omp decorated for loops.')
